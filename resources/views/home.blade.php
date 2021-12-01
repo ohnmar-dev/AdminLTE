f3b4133dc9e33c590407012303c095dc4fd9393d
Ohnmar <ohnmarhtay2021.aa@gmail.com>
2021-12-01 20:01:43 +0630

    created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('adminlte::page',['iFrameEnabled'=>true])
+@extends('adminlte::page')
 
 @section('title', 'Dashboard')
 
@@ -7,13 +7,42 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+<div class="container">
+        <div class="row">
+            <div class="col-md-7 offset-3 mt-4">
+                <div class="card-body">
+
+                    <form method=”POST” action="">
+                        @csrf
+                        <div class="form-group">
+                            <textarea class="form-control" name="summernote" id="summernote"></textarea>
+                        </div>
+                        <button type=”submit” class="btn btn-danger btn-block">Save</button>
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
 
 @section('css')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
 
 @section('js')
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+<script type="text/javascript">
+    $('#summernote').summernote({
+        height:100
+    });
+</script>
     <script> console.log('Hi!'); </script>
 @stop
